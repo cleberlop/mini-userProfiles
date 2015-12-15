d1960dd4659a8cb94075c866dba8b70cb959ab82
Cleber Albuquerque <cleber.lop@gmail.com>
2015-12-15 13:56:39 -0700

    finished

---
+++ controller.js
@@ -0,0 +1,11 @@
+angular.module("userProfiles").controller("MainController", function($scope, MainService) {
+
+
+  $scope.getUsers = function() {
+    $scope.users = MainService.getUsers();
+  };
+  
+  $scope.getUsers();
+
+
+});
